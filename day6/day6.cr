4f3adbf2aba31f569ed08840967c3111cc08e053
Jeffrey Massung <massung@gmail.com>
2022-12-05 23:30:56 -0600

    Tweak.

--- day6/day6.cr
+++ day6/day6.cr
@@ -5,8 +5,6 @@ def find_marker(s : String, n : Int)
     ns = [0] * 26
     s[i - n...i].chars.all? do |c|
       i = c.ord - 'a'.ord
-
-      # bust out as soon as a bucket has more than 1 character
       ns[i] += 1
       ns[i] < 2
     end
