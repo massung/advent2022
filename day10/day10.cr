73c5aadf5d2b712c8ca972b11f7eee2a2acd0a22
Jeffrey Massung <massung@gmail.com>
2022-12-09 23:42:18 -0600

    Day 10.

--- day10/day10.cr
+++ day10/day10.cr
@@ -1,3 +1,43 @@
-File.each_line("test.txt") do |line|
-  # TODO:
+SIGNALS = [20, 60, 100, 140, 180, 220]
+
+class Day10
+  def initialize(file : String)
+    @cycle = 0
+    @x = 1
+    @signal = 0
+
+    File.each_line(file) do |line|
+      cmd = line.split
+
+      case cmd[0]
+      when "noop"; step
+      when "addx"; step; step; @x += cmd[1].to_i
+      else         raise Exception.new("ACK")
+      end
+    end
+
+    puts @signal
+  end
+
+  def step
+    @cycle += 1
+
+    # signal tally
+    @signal += @cycle * @x if SIGNALS.includes?(@cycle)
+
+    # draw pixel
+    pixel = (@cycle - 1) % 40
+
+    # is pixel lit?
+    if (@x - 1..@x + 1).includes?(pixel)
+      print "#"
+    else
+      print "."
+    end
+
+    # last pixel?
+    print "\n" if pixel == 39
+  end
 end
+
+Day10.new("data.txt")
