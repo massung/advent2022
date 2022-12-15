71318c311f072c40649a34cd74d8f205530f4500
Jeffrey Massung <massung@gmail.com>
2022-12-15 01:08:37 -0600

    Better region merging.

--- day15/day15.cr
+++ day15/day15.cr
@@ -32,45 +32,44 @@ class Day15
     (x1..x2)
   end
   
-  def visible_ranges(y : Int32) : Array(Range(Int32, Int32))
-    @sensors.map { |s| visible_range(s, y) }.sort_by! { |r| -r.begin }
+  def ranges_overlap?(r : Range(Int32, Int32), e : Range(Int32, Int32)) : Bool
+    e.includes?(r.begin) || e.includes?(r.end) || r.includes?(e.begin) || r.includes?(e.end)
+  end
+  
+  def merged_ranges(y : Int32) : Array(Range(Int32, Int32))
+    rs = Array(Range(Int32, Int32)).new(@sensors.size)
+    
+    @sensors.each do |sensor|
+      r = visible_range(sensor, y)
+      
+      # does this overlap an existing range?
+      if i = rs.index { |e| ranges_overlap?(r, e) }
+        rs[i] = Math.min(r.begin, rs[i].begin)..Math.max(r.end, rs[i].end)
+      else
+        rs << r
+      end
+    end
+    
+    rs.sort_by! &.begin
   end
   
   def part1(y : Int32) : Int32
-    rs = visible_ranges(y)
-    ms = [rs.pop]
-    
-    # merge ranges that overlap
-    until rs.empty?
-      r = rs.pop
-      
-      # replace old range with an extended one
-      ms.index { |o| r.begin <= o.end && r.end > o.end }.try do |i|
-        m = ms.delete_at(i)
-        ms << (m.begin..r.end)
-      end
-    end
-
-    # count ranges exclude beacons
-    ms.sum do |r|
+    merged_ranges(y).sum do |r|
       r.size - @beacons.count { |b| b[:y] == y && r.includes?(b[:x]) }
     end
   end
   
   def part2(extent : Int32) : BigInt?
     (0..extent).each do |y|
-      rs = visible_ranges(y)
+      rs = merged_ranges(y).each
       x = 0
 
-      until x > extent || rs.empty?
-        r = rs.pop
-        
-        # advance to the end of this sensor's range
-        x = r.end + 1 if r.begin <= x <= r.end
+      while x <= extent
+        case r = rs.next
+        in Iterator::Stop; return BigInt.new(x) * 4000000 + y 
+        in Range(Int32, Int32); x = r.end + 1 if r.includes?(x)
+        end
       end
-      
-      # solution found if all regions exhausted      
-      return BigInt.new(x) * 4000000 + y if rs.empty?
     end
   end
 end
